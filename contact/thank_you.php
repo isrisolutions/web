c3acb2fef86a56476a1891bf55a26b23f40db7f9
Abhinav Thukral <thukral.abhinav@gmail.com>
2020-01-19 13:37:44 +0530

    First commit

---
+++ contact/thank_you.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Isri Solutions</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="http://abhinavthukral.in/lab/Manuscript/css/master-theme-sun.css">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:200,300,300i,400,400i,500,600,700,800,900|Roboto:100,300,300i,400,400i,500,700,900" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+	<script
+  src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+  integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E="
+  crossorigin="anonymous"></script>
+  <script type="text/javascript" src="http://abhinavthukral.in/lab/Manuscript/css/master.js"></script>
+</head>
+<body>
+<header class="fixed">
+	<div class="content">
+		<div class="header-logo">
+			<img src="../Logo.svg" class="small-fit"><div class="text-logo">isri solutions <div class="beta">PVT. LTD.</div></div>
+		</div>
+	</div>
+</header>
+<section class="title-section header-fixed">
+	<div class="half-content">
+		<h1 class="white">Thank You</h1>
+	</div>
+</section>
+<section>
+	<div class="half-content">
+		<p>We have received your request. We will get back to you shortly. Thank you!</p>
+	</div>
+</section>
+<footer>
+      <div class="content">
+        <p class="madeby center">&copy; Isri Solutions Pvt. Ltd. 2019</p>
+      </div>
+</footer>
+</body>
+</html>
